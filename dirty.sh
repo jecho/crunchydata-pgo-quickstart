3c3f2f13532f0ba4704e4ffa5ad28ff5b33020b4
Jecho <jecho@users.noreply.github.com>
2019-01-08 09:56:24 -0800

    Updated dirty.sh
    removed comments

--- dirty.sh
+++ dirty.sh
@@ -6,12 +6,10 @@ export COROOT=$HOME/staging/postgres-operator
 
 export CO_IMAGE_PREFIX=crunchydata
 export CO_IMAGE_TAG=centos7-3.4.0
-Install the pgo client
 
 wget https://github.com/CrunchyData/postgres-operator/releases/download/3.4.0/pgo-mac
 chmod +x pgo
 mv pgo-mac /usr/local/bin/pgo
-Stand up variables for our pgo client
 
 export PGO_CA_CERT=$COROOT/conf/postgres-operator/server.crt
 export PGO_CLIENT_CERT=$COROOT/conf/postgres-operator/server.crt
@@ -26,4 +24,3 @@ export HTTPS=https://
 export CO_APISERVER_URL=$(kubectl get svc ${CO_APISERVER_NAME} --output=jsonpath='{range .status.loadBalancer.ingress[0]}{.ip}') 
 export CO_APISERVER_PORT=8443
 export CO_APISERVER_URL=${HTTPS}${CO_APISERVER_URL}:${CO_APISERVER_PORT}
-
